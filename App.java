3c5816014b8e6e467f75d07238c8dca1b541a9ac
bramos013 <brunoramos013@gmail.com>
2020-11-19 20:58:32 -0300

    Criando Bug

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-public class App {
+public class Dart {
     public static void main(String[] args) {
         System.out.println("Não esqueçam de se inscrever no canal, por hoje é só pessoal!");
     }
