308cee97b067a074d0d683085e17c9f5e8808c93
bramos013 <brunoramos013@gmail.com>
2020-11-19 20:26:02 -0300

    Corrigindo App

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-public class Dart {
+public class App {
     public static void main(String[] args) {
         System.out.println("Não esqueçam de se inscrever no canal, por hoje é só pessoal!");
     }
